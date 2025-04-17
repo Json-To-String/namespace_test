475bf18bec6f8c0f169f9b84650e745ec1aca8e1
Jason Pruitt <55477390+Json-To-String@users.noreply.github.com>
2025-04-17 16:22:41 -0400

    layout change tested and should work?

--- module_1/file_creater.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class FileCreator:
-    def create(filename: str):
-        path_exists = os.path.exists(filename)
-        if not path_exists:
-            os.makedirs(filename)
-        else:
-            print(f"Something went wrong trying to make {filename}")
